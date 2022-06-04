01b5b0e1292fa58269d3b375b2ec84a1d07fe1b0
Moukhtar-youssef <moukhtar.youssef06@gmail.com>
2022-06-04 17:51:50 +0400

    making the calculator look awesome

---
+++ js/main.js
@@ -0,0 +1,28 @@
+let input = document.querySelector('#input');
+let output =  document.querySelector('#output');
+let buttons = Array.from(document.querySelectorAll("button"));
+buttons.map(buttons =>{
+    buttons.addEventListener('click', (e) =>{
+        switch(e.target.innerText){
+            case 'AC':
+                input.innerText = ""
+                output.innerText = ""
+                break
+            case 'Del':
+                if(input.innerText){
+                    input.innerText = input.innerText.slice(0 , -1);
+                }
+                break
+            case '=':
+                try{
+                    output.innerText = eval(input.innerText);
+                }
+                catch{
+                    output.innerText = 'Error!'
+                }
+                break
+            default:
+                input.innerText += e.target.innerText
+        }
+    })
+})
