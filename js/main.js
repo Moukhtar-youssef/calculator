25b123bb9a83e567c1cf668a6d1fe3597282b014
Moukhtar-youssef <moukhtar.youssef06@gmail.com>
2022-09-17 23:41:10 +0400

    adding some featurs in the js fie:

--- js/main.js
+++ js/main.js
@@ -1,29 +1,31 @@
-let input = document.querySelector('#input');
-let output =  document.querySelector('#output');
+"use strict";
+let input = document.querySelector("#input");
+let output = document.querySelector("#output");
 let buttons = Array.from(document.querySelectorAll("button"));
-buttons.map(buttons =>{
-    buttons.addEventListener('click', (e) =>{
-        switch(e.target.innerText){
-            case 'AC':
-                input.innerText = ""
-                output.innerText = ""
-                break
-            case 'Del':
-                if(input.innerText){
-                    input.innerText = input.innerText.slice(0 , -1);
-                    output.innerText = ""
-                }
-                break
-                case '=':
-                try{
-                    output.innerText = eval(input.innerText);
-                }
-                catch{
-                    output.innerText = 'Error!'
-                }
-                break
-            default:
-                input.innerText += e.target.innerText
+buttons.map((buttons) => {
+  buttons.addEventListener("click", (e) => {
+    switch (e.target.innerText) {
+      case "AC":
+        input.innerText = "";
+        output.innerText = "";
+        break;
+      case "Del":
+        if (input.innerText) {
+          input.innerText = input.innerText.slice(0, -1);
+          output.innerText = "";
         }
-    })
-})
+        break;
+      case "=":
+        try {
+          output.innerText = eval(input.innerText);
+        } catch (_a) {
+          output.innerText = "Error!";
+        }
+        break;
+      default:
+        if (input.innerText.length <= 12) {
+          input.innerText += e.target.innerText;
+        }
+    }
+  });
+});
